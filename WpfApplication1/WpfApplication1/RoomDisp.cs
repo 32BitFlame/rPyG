7bf96be2f52d3259c0c3cb3def28f8b228395ff7
Flame <benjamess3@hotmail.com>
2019-04-17 19:29:32 -0400

    Worked on GUI

---
+++ WpfApplication1/WpfApplication1/RoomDisp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    public class RoomDisp
+    {
+        private string jsonData;
+        public RoomDisp(string jsondata)
+        {
+            this.jsonData = jsondata;
+        }
+
+    }
+}
