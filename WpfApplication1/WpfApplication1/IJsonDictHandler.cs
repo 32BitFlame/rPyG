37885841942a2468f5d4c6225bddb1cc32f70045
32BitFlame <47258322+32BitFlame@users.noreply.github.com>
2019-04-19 19:39:18 -0400

    Created Json Dict. Handler Interface

---
+++ WpfApplication1/WpfApplication1/IJsonDictHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public interface IJsonDictDataHandler
+    {
+        Dictionary<string, string> Dict { get; set; }
+
+        void Load(string fileAddress);
+
+        Dictionary<string, string> Dump();
+
+    }
+}
